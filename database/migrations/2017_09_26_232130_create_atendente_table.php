08b49683668ec54bd526346bef88349172532dd8
MariaQueiroz <138670@upf.br>
2017-10-01 15:31:53 -0300

    migrations

---
+++ database/migrations/2017_09_26_232130_create_atendente_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtendentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atendentes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome',50);
+            $table->string('sobrenome',50);
+            $table->string('telefone',20);
+            $table->string('email',50);
+            $table->string('senha',20);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atendentes');
+    }
+}
---
+++ database/migrations/2017_09_26_232130_create_fornecedor_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFornecedorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fornecedor', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome',50);
+            $table->string('telefone',20);
+            $table->string('endereco',150);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fornecedor');
+    }
+}
